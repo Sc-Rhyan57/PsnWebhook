a382b85132dd68031da932fe726d4b73f136db31
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-03-02 22:35:00 -0300

    feat: upload files via Android Dev Tools

---
+++ app/src/main/java/com/psnwebhook/ui/Theme.kt
@@ -0,0 +1,27 @@
+package com.psnwebhook.ui
+
+import androidx.compose.ui.graphics.Color
+
+object C {
+    val Bg       = Color(0xFF00041A)
+    val Surface  = Color(0xFF000D35)
+    val Card     = Color(0xFF001240)
+    val CardAlt  = Color(0xFF001A55)
+    val Border   = Color(0xFF00275A)
+    val Primary  = Color(0xFF00439C)
+    val Accent   = Color(0xFF0070D1)
+    val Blue2    = Color(0xFF003087)
+    val White    = Color(0xFFE8EAF6)
+    val Muted    = Color(0xFF6B7DB3)
+    val Success  = Color(0xFF00C896)
+    val Error    = Color(0xFFEF4444)
+    val Warning  = Color(0xFFF59E0B)
+    val Gold     = Color(0xFFFFD700)
+    val Silver   = Color(0xFFC0C0C0)
+    val Bronze   = Color(0xFFCD7F32)
+    val PS5White = Color(0xFFF0F0F0)
+    val Cyan     = Color(0xFF00D4FF)
+    val GradTop  = Color(0xFF00041A)
+    val GradMid  = Color(0xFF000D35)
+    val GradBot  = Color(0xFF00041A)
+}
